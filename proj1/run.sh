677ce4d1ad0f9ed35e8a8712106f8b92a7648e61
Brian Park <bcpark@ncsu.edu>
2022-10-02 01:39:15 -0400

    finish proj1

--- proj1/run.sh
+++ proj1/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-for file in test/*gee.txt
-do
-  echo "Running gee.py for $file"
-  python3 gee.py $file
+cd test/text/
+for filename in *.txt; do
+  file="${filename%%.*}"
+  echo "Running $file"
+  cd ../..
+  python3 gee.py test/text/$file.gee.txt > test/out/$file.out
+  diff -b test/out/$file.out test/out/$file-out.txt
+  rm test/out/$file.out
+  cd test/text/
 done
