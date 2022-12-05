3d30a1ab80be8c78373ebf0c43806b8237a18ef5
Brian Park <bcpark@ncsu.edu>
2022-12-04 21:22:34 -0500

    finish final project

---
+++ proj3/extracredit.md
@@ -0,0 +1,30 @@
+# CSC 512 Project 3 Extra Credit
+Brian Park
+
+1. Here is the visualization 
+![](drctprof_example.png)
+
+2. I used the overflow bit solution with the help from DynamoRIO flag (`EFLAGS_READ_OF`) listed [here](https://dynamorio.org/globals_e.html). That was the easiest solution, and it can detect nearly the same outputs, with just fewer lines of code! I prefer this solution better as it is neater and abstracts all teh details away.
+
+```cpp
+static inline bool
+IntegerOverflow(Instruction *instr, uint64_t flagsValue)
+{
+    Operand srcOpnd0 = instr->getSrcOperand(0);
+    Operand srcOpnd1 = instr->getSrcOperand(1);
+    Operand dstOpnd = instr->getDstOperand(0);
+    std::bitset<64> bitFlagsValue(flagsValue);
+    OperatorType optype = instr->getOperatorType();
+
+    if (optype == OperatorType::kOPadd || 
+        optype == OperatorType::kOPsub || 
+        optype == OperatorType::kOPshl) {
+        return (flagsValue & EFLAGS_READ_OF) == 0;
+    } else {
+        return false;
+    }
+}
+```
+
+
+3. I think using flag bits is more efficient. We can utilize hardware bits to do simple detection using bitwise operators. Doing the algorithm from slide 12 incurs too much overhead, especially if you're trying to detect overflow for a very large program. How much is that overhead? I quickly measured it, but it's 3.131s for the flag implementation and 3.719s for my hand made implementation from lecture. It's not much of a difference, but it indicates that my code is inefficienct for detecting integer overflow manually.
--- proj3/src/profiler.cpp
+++ proj3/src/profiler.cpp
@@ -3,6 +3,7 @@
 #include <bitset>
 #include <cstdint>
 #include <cmath>
+#include <limits>
 #include "profiler.h"
 #include "drcctlib_vscodeex_format.h"
 
@@ -17,28 +18,105 @@ using namespace DrCCTProf;
 
 namespace runtime_profiling {
 
-static inline int64_t
-GetOpndIntValue(Operand opnd)
+static inline int64_t GetOpndIntValue(Operand opnd)
 {
-    if (opnd.type == OperandType::kImmediateFloat ||
-        opnd.type == OperandType::kImmediateDouble ||
-        opnd.type == OperandType::kUnsupportOpnd || opnd.type == OperandType::kNullOpnd) {
+    if (opnd.type == OperandType::kImmediateFloat
+        || opnd.type == OperandType::kImmediateDouble
+        || opnd.type == OperandType::kUnsupportOpnd
+        || opnd.type == OperandType::kNullOpnd) {
         return 0;
     }
     int64_t value = 0;
     switch (opnd.size) {
-    case 1: value = static_cast<int64_t>(opnd.value.valueInt8); break;
-    case 2: value = static_cast<int64_t>(opnd.value.valueInt16); break;
-    case 4: value = static_cast<int64_t>(opnd.value.valueInt32); break;
-    case 8: value = static_cast<int64_t>(opnd.value.valueInt64); break;
-    default: break;
+    case 1:
+        value = static_cast<int64_t>(opnd.value.valueInt8);
+        break;
+    case 2:
+        value = static_cast<int64_t>(opnd.value.valueInt16);
+        break;
+    case 4:
+        value = static_cast<int64_t>(opnd.value.valueInt32);
+        break;
+    case 8:
+        value = static_cast<int64_t>(opnd.value.valueInt64);
+        break;
+    default:
+        break;
     }
     return value;
 }
 
+static inline int64_t getIntMax(uint8_t bytes)
+{
+    switch (bytes) {
+    case 1:
+        return INT8_MAX;
+    case 2:
+        return INT16_MAX;
+    case 4:
+        return INT32_MAX;
+    case 8:
+        return INT64_MAX;
+    default:
+        return 0;
+    }
+}
+
+static inline int64_t getIntMin(uint8_t bytes)
+{
+    switch (bytes) {
+    case 1:
+        return INT8_MIN;
+    case 2:
+        return INT16_MIN;
+    case 4:
+        return INT32_MIN;
+    case 8:
+        return INT64_MIN;
+    default:
+        return 0;
+    }
+}
+
+static inline bool safe_add(int64_t a, int64_t b, uint8_t bytes)
+{
+    int64_t intMax = getIntMax(bytes);
+    int64_t intMin = getIntMin(bytes);
+
+    if (a > 0 && b > intMax - a)
+        return true;
+    else if (a < 0 && b < intMin - a)
+        return true;
+    else
+        return false;
+}
+
+static inline bool safe_sub(int64_t a, int64_t b, uint8_t bytes)
+{
+    int64_t intMax = getIntMax(bytes);
+    int64_t intMin = getIntMin(bytes);
+
+    if (a > 0 && b < intMin + a)
+        return true;
+    else if (a < 0 && b > intMax + a + 1)
+        return true;
+    else
+        return false;
+}
+
+static inline bool safe_shl(int64_t a, int64_t b, uint8_t bytes)
+{
+    __int128_t wideResult = a;
+    wideResult = wideResult << b;
+
+    return (bytes == 1 && (wideResult > INT8_MAX || wideResult < INT8_MIN))
+           || (bytes == 2 && (wideResult > INT16_MAX || wideResult < INT16_MIN))
+           || (bytes == 4 && (wideResult > INT32_MAX || wideResult < INT32_MIN))
+           || (bytes == 8 && (wideResult > INT64_MAX || wideResult < INT64_MIN));
+}
+
 // implement your algorithm in this function
-static inline bool
-IntegerOverflow(Instruction *instr, uint64_t flagsValue)
+static inline bool IntegerOverflow(Instruction* instr, uint64_t flagsValue)
 {
     Operand srcOpnd0 = instr->getSrcOperand(0);
     Operand srcOpnd1 = instr->getSrcOperand(1);
@@ -46,18 +124,32 @@ IntegerOverflow(Instruction *instr, uint64_t flagsValue)
     std::bitset<64> bitFlagsValue(flagsValue);
     OperatorType optype = instr->getOperatorType();
 
-    if (optype == OperatorType::kOPadd || 
-        optype == OperatorType::kOPsub || 
-        optype == OperatorType::kOPshl) {
-        return (flagsValue & EFLAGS_READ_OF) == 0;
+    // OVERFLOW BIT SOLUTION STARTS HERE
+    // if (optype == OperatorType::kOPadd ||
+    //     optype == OperatorType::kOPsub ||
+    //     optype == OperatorType::kOPshl) {
+    //     return (flagsValue & EFLAGS_READ_OF) == 0;
+    // } else {
+    //     return false;
+    // }
+    // OVERFLOW BIT SOLUTION STARTS HERE
+
+    int64_t src0Val = GetOpndIntValue(srcOpnd0);
+    int64_t src1Val = GetOpndIntValue(srcOpnd1);
+
+    if (optype == OperatorType::kOPadd) {
+        return safe_add(src0Val, src1Val, dstOpnd.size);
+    } else if (optype == OperatorType::kOPsub) {
+        return safe_sub(src0Val, src1Val, dstOpnd.size);
+    } else if (optype == OperatorType::kOPshl) {
+        return safe_shl(src0Val, src1Val, dstOpnd.size);
     } else {
         return false;
     }
 }
 
-void
-OnAfterInsExec(Instruction *instr, context_handle_t contxt, uint64_t flagsValue,
-                        CtxtContainer *ctxtContainer)
+void OnAfterInsExec(Instruction* instr, context_handle_t contxt, uint64_t flagsValue,
+                    CtxtContainer* ctxtContainer)
 {
     if (IntegerOverflow(instr, flagsValue)) {
         ctxtContainer->addCtxt(contxt);
@@ -83,7 +175,7 @@ OnAfterInsExec(Instruction *instr, context_handle_t contxt, uint64_t flagsValue,
         Operand srcOpnd0 = instr->getSrcOperand(0);
         Operand srcOpnd1 = instr->getSrcOperand(1);
         Operand dstOpnd = instr->getDstOperand(0);
-        
+
         std::bitset<64> bitFlagsValue(flagsValue);
 
         if (IntegerOverflow(instr, flagsValue)) {
@@ -113,22 +205,22 @@ OnAfterInsExec(Instruction *instr, context_handle_t contxt, uint64_t flagsValue,
     }
 }
 
-void
-OnBeforeAppExit(CtxtContainer *ctxtContainer)
+void OnBeforeAppExit(CtxtContainer* ctxtContainer)
 {
-    Profile::profile_t *profile = new Profile::profile_t();
+    Profile::profile_t* profile = new Profile::profile_t();
     profile->add_metric_type(1, "", "integer overflow occurrence");
 
     std::vector<context_handle_t> list = ctxtContainer->getCtxtList();
     for (size_t i = 0; i < list.size(); i++) {
-        inner_context_t *cur_ctxt = drcctlib_get_full_cct(list[i]);
+        inner_context_t* cur_ctxt = drcctlib_get_full_cct(list[i]);
         profile->add_sample(cur_ctxt)->append_metirc((uint64_t)1);
         drcctlib_free_full_cct(cur_ctxt);
     }
     profile->serialize_to_file("integer-overflow-profile.drcctprof");
     delete profile;
 
-    file_t profileTxt = dr_open_file("integer-overflow-profile.txt", DR_FILE_WRITE_OVERWRITE | DR_FILE_ALLOW_LARGE);
+    file_t profileTxt = dr_open_file("integer-overflow-profile.txt",
+                                     DR_FILE_WRITE_OVERWRITE | DR_FILE_ALLOW_LARGE);
     DR_ASSERT(profileTxt != INVALID_FILE);
     for (size_t i = 0; i < list.size(); i++) {
         dr_fprintf(profileTxt, "INTEGER OVERFLOW\n");
@@ -139,5 +231,4 @@ OnBeforeAppExit(CtxtContainer *ctxtContainer)
     }
     dr_close_file(profileTxt);
 }
-
 }
